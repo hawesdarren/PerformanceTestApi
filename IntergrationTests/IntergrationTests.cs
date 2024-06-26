f0562f405192ec380c2926ff1a8cb7f34e86751f
darrenhawes <darren.hawes@kiwibank.co.nz>
2024-06-27 10:35:18 +1200

    wip

--- IntergrationTests/IntergrationTests.cs
+++ IntergrationTests/IntergrationTests.cs
@@ -45,7 +45,7 @@ namespace IntergrationTests
             //Set initial date counter
             int dateCounter = 0;
             foreach (var weatherResponse in response) {
-                DateOnly date = DateOnly.FromDateTime(DateTime.Now.AddDays(dateCounter));
+                DateOnly date = DateOnly.FromDateTime(DateTime.Now.AddDays(dateCounter).ToLocalTime());
                 System.Console.WriteLine("Date: " + date);
                 string formatedDate = date.ToString("yyyy-MM-dd");
                 weatherResponse.date.Should().Be(formatedDate);
